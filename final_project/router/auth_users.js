0381704bfd311810db11e908d2152934f8d023a5
romainmaubry <80488466+romainmaubry@users.noreply.github.com>
2024-07-21 17:58:24 -0400

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -24,16 +24,56 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+    return res.status(404).json({ message: "Error logging in" });
+  }
+  if (authenticatedUser(username, password)) {
+    let accessToken = jwt.sign({
+      data: password
+    }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {
+      accessToken, username
+    };
+    return res.status(200).send("User successfully logged in");
+  } else {
+    return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }  
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+   // Extract email parameter and find users with matching email
+    const isbn = req.params.isbn
+    const review=req.param.reviews
+    let filtered_books = Object.values(books).filter((book) => book.isbn === isbn);
+    
+    if (filtered_books.length > 0) {
+        // Select the first matching user and update attributes if provided
+        let filtered_book = filtered_books[0];
+        
+       filtered_book.array.forEach(book => {
+        book.review=review
+       });
+        
+        // Replace old user entry with updated user
+        books = books.filter((book) => book.isbn != isbn);
+        books.push(filtered_books);
+        
+        // Send success message indicating the user has been updated
+        res.send(`Book with the name ${name} updated.`);
+    } else {
+        // Send error message if no user found
+        res.send("Unable to find user!");
+    }
+  
+  //return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
-
